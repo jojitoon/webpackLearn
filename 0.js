52d92195cee6a2416c619467a4eccedafd8dc78c
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-09-30 10:13:36 +0100

    Updates

---
+++ 0.js
@@ -0,0 +1,21 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"./src/assets/pizza.jpg":
+/*!******************************!*\
+  !*** ./src/assets/pizza.jpg ***!
+  \******************************/
+/*! no static exports found */function(e,t,n){e.exports=n.p+"6bfdd4fa50ca3600518a1dc92dfa131c.jpg"},"./src/components/PizzaImage/PizzaImage.css":
+/*!**************************************************!*\
+  !*** ./src/components/PizzaImage/PizzaImage.css ***!
+  \**************************************************/
+/*! no static exports found */function(e,t,n){e.exports={PizzaImage:"_2gKj0XFjCcvH0h5TUMIDIO",PizzaImg:"_3RIyoMEaoi0cMtVWhOiWz1"}},"./src/components/PizzaImage/PizzaImage.js":
+/*!*************************************************!*\
+  !*** ./src/components/PizzaImage/PizzaImage.js ***!
+  \*************************************************/
+/*! exports provided: default */function(e,t,n){"use strict";n.r(t);var o=n(/*! react */"./node_modules/react/index.js"),r=n.n(o),a=(n(/*! ./pizza.scss */"./src/components/PizzaImage/pizza.scss"),n(/*! ./PizzaImage.css */"./src/components/PizzaImage/PizzaImage.css")),c=n.n(a),i=n(/*! ../../assets/pizza.jpg */"./src/assets/pizza.jpg"),s=n.n(i);t.default=function(){return r.a.createElement("div",{className:c.a.PizzaImage},r.a.createElement("img",{src:s.a,className:c.a.PizzaImg}),r.a.createElement("p",null,"Testing Scss"))}},"./src/components/PizzaImage/pizza.scss":
+/*!**********************************************!*\
+  !*** ./src/components/PizzaImage/pizza.scss ***!
+  \**********************************************/
+/*! no static exports found */function(e,t,n){},"./src/containers/Pizza.js":
+/*!*********************************!*\
+  !*** ./src/containers/Pizza.js ***!
+  \*********************************/
+/*! exports provided: default */function(e,t,n){"use strict";n.r(t);var a=n(/*! react */"./node_modules/react/index.js"),c=n.n(a),i=n(/*! ../components/PizzaImage/PizzaImage */"./src/components/PizzaImage/PizzaImage.js");function o(e){return(o="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e})(e)}function s(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}function u(e,t){return!t||"object"!==o(t)&&"function"!=typeof t?function(e){if(void 0!==e)return e;throw new ReferenceError("this hasn't been initialised - super() hasn't been called")}(e):t}function f(e){return(f=Object.setPrototypeOf?Object.getPrototypeOf:function(e){return e.__proto__||Object.getPrototypeOf(e)})(e)}function p(e,t){return(p=Object.setPrototypeOf||function(e,t){return e.__proto__=t,e})(e,t)}var r=function(e){function t(){return function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),u(this,f(t).apply(this,arguments))}var n,o,r;return function(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function");e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,writable:!0,configurable:!0}}),t&&p(e,t)}(t,a["Component"]),n=t,(o=[{key:"render",value:function(){return c.a.createElement("div",null,c.a.createElement("h1",null,"The Pizza"),c.a.createElement(i.default,null))}}])&&s(n.prototype,o),r&&s(n,r),t}();t.default=r}}]);
